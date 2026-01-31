e80356f47da5c9bbb2e06d0f539bef3771d03c66
LegendaryForge <LegendaryForge@users.noreply.github.com>
2026-01-30 18:37:50 -0700

    chore: remove LegendaryContent dogfood code from LegendaryStormseeker

--- src/main/java/io/github/legendaryforge/legendary/content/encounter/toystorm/ToyStormEndSummary.java
+++
@@ -1,5 +0,0 @@
-package io.github.legendaryforge.legendary.content.encounter.toystorm;
-
-import java.util.UUID;
-
-public record ToyStormEndSummary(UUID instanceId, int participantsAtEnd, EncounterPhase finalPhase) {}
